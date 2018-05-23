93669f91764189756eab8d91b10e2c70bea1f130
Jaeguins <jsu4838@naver.com>
2018-05-23 14:53:10 +0900

    opacity controller add

--- src/screen/GraphicViewer.java
+++ src/screen/GraphicViewer.java
@@ -1,132 +1,146 @@
 package screen;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
-import java.awt.image.ImageObserver;
-import java.text.AttributedCharacterIterator;
+import java.io.File;
+import java.io.IOException;
 
 public class GraphicViewer extends Canvas implements Runnable,MouseListener{
-	int size;
-	BufferedImage bf;
-	BufferedImg[] imgBuffer;
-	boolean[] isBtnBuf;
-	boolean firstPainted=false;
-	Thread thr;
-	public GraphicViewer(int bufferSize,int width,int height) {
-		this.addMouseListener(this);
-		this.size=bufferSize;
-		this.setSize(width,height);
-		this.setBackground(Color.white);
-		imgBuffer=new BufferedImg[size];
-		isBtnBuf=new boolean[size];
-		for(int i=0;i<size;i++) {
-			isBtnBuf[i]=false;
-		}
-	}
+    int size;
+    BufferedImage bf;
+    BufferedImg[] imgBuffer;
+    boolean[] imgCheck;
+    boolean firstPainted=false;
+    Thread thr;
+    public GraphicViewer(int bufferSize,int width,int height) {
+        this.addMouseListener(this);
+        this.size=bufferSize;
+        this.setSize(width,height);
+        this.setBackground(Color.white);
+        imgBuffer=new BufferedImg[size];
+        imgCheck=new boolean[size];
+        for(int i=0;i<size;i++) {
+            imgCheck[i]=false;
+        }
+    }
 
-	public int addButton(BufferedImg btn) {
-		for(int i=0;i<size;i++) {
-			if(!isBtnBuf[i]) {
-				imgBuffer[i]=btn;
-				isBtnBuf[i]=true;
-				btn.setId(i);
-				return i;
-			}
-		}
-		return -1;
-	}
-	public BufferedImg getButtonById(int index) {
-		return imgBuffer[index];
-	}
-	public void deleteButton(int index) {
-		imgBuffer[index]=null;
-		isBtnBuf[index]=false;
-	}
-	public void paint(Graphics g) {
-		if(!firstPainted) {
-			firstPainted=true;
-			thr=new Thread(this);
-			thr.start();
-		}
-		bf= new BufferedImage( this.getWidth(), this.getHeight(),BufferedImage.TYPE_INT_RGB);
-		Graphics2D g2d=(Graphics2D) bf.getGraphics();
-		g2d.setColor(Color.white);
-		g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
-		for(int i=0;i<size;i++) {
-			if(isBtnBuf[i]) {
-				g2d.drawImage(imgBuffer[i].getImg(),imgBuffer[i].getX(),imgBuffer[i].getY(),imgBuffer[i].getWidth(),imgBuffer[i].getHeight(),this);
-				if(imgBuffer[i].getFont()!=null)
-				g2d.setFont(imgBuffer[i].getFont());
-				g2d.setColor(imgBuffer[i].getTextColor());
-				g2d.drawString(imgBuffer[i].getText(),(int) (imgBuffer[i].getX()+0.76*imgBuffer[i].getFont().getSize()/2),(int) (imgBuffer[i].getY()+imgBuffer[i].getHeight()/2+0.76*imgBuffer[i].getFont().getSize()/2));
+    public int addButton(BufferedImg btn) {
+        for(int i=0;i<size;i++) {
+            if(!imgCheck[i]) {
+                imgBuffer[i]=btn;
+                imgCheck[i]=true;
+                btn.setId(i);
+                return i;
+            }
+        }
+        return -1;
+    }
+    public BufferedImg getButtonById(int index) {
+        return imgBuffer[index];
+    }
+    public void deleteButton(int index) {
+        imgBuffer[index]=null;
+        imgCheck[index]=false;
+    }
+    public void paint(Graphics g) {
+        if(!firstPainted) {
+            firstPainted=true;
+            thr=new Thread(this);
+            thr.start();
+        }
+        bf= new BufferedImage( this.getWidth(), this.getHeight(),BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d=(Graphics2D) bf.getGraphics();
+        g2d.setColor(Color.white);
+        g2d.fillRect(0, 0, this.getWidth(), this.getHeight());
+        for(int i=0;i<size;i++) {
+            if(imgCheck[i]) {
+                g2d.setComposite(AlphaComposite.SrcOver.derive(imgBuffer[i].getAlpha()));
+                g2d.drawImage(imgBuffer[i].getImg(),imgBuffer[i].getX(),imgBuffer[i].getY(),imgBuffer[i].getWidth(),imgBuffer[i].getHeight(),this);
+                g2d.setFont(imgBuffer[i].getFont());
+                g2d.setColor(imgBuffer[i].getTextColor());
+                g2d.drawString(imgBuffer[i].getText(),(int) (imgBuffer[i].getX()+0.76*imgBuffer[i].getFont().getSize()/2),(int) (imgBuffer[i].getY()+imgBuffer[i].getHeight()/2+0.76*imgBuffer[i].getFont().getSize()/2));
 
-			}	
-		}
-		
-		g.drawImage(bf,0,0,null);
-		
-	}
-	@Override
-	public void run() {
-		while(true) {
-			try {
-				Thread.sleep(40);
-				paint(this.getGraphics());	
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
+            }	
+        }
 
-	}
+        g.drawImage(bf,0,0,null);
 
-	@Override
-	public void mouseClicked(MouseEvent arg0) {
+    }
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(40);
+                paint(this.getGraphics());	
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
-	}
+    }
 
-	@Override
-	public void mouseEntered(MouseEvent arg0) {
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
 
-	}
+    }
 
-	@Override
-	public void mouseExited(MouseEvent arg0) {
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
 
-	}
+    }
 
-	@Override
-	public void mousePressed(MouseEvent arg0) {
-		int tX=arg0.getX();
-		int tY=arg0.getY();
-		for(int i=0;i<size;i++) {
-			if(isBtnBuf[i]) {
-				if(imgBuffer[i].getX()<tX&&tX<imgBuffer[i].getX()+imgBuffer[i].getWidth()&&imgBuffer[i].getY()<tY&&tY<imgBuffer[i].getY()+imgBuffer[i].getHeight()) {
-					imgBuffer[i].action();
-				}
-			}
-		}
-		System.out.println(tX+" "+tY);
-	}
+    @Override
+    public void mouseExited(MouseEvent arg0) {
 
-	@Override
-	public void mouseReleased(MouseEvent arg0) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+        int tX=arg0.getX();
+        int tY=arg0.getY();
+        for(int i=size-1;i>=0;i--) {
+            if(imgCheck[i]) {
+                if(imgBuffer[i].getX()<tX&&tX<imgBuffer[i].getX()+imgBuffer[i].getWidth()&&imgBuffer[i].getY()<tY&&tY<imgBuffer[i].getY()+imgBuffer[i].getHeight()) {
+                    imgBuffer[i].action();
+                    break;
+                }
+            }
+        }
+        System.out.println(tX+" "+tY);
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
 
 
-	}
+    }
 }
 
 class BufferedImg implements Runnable{
-	private int x,y,width,height;
-	private Image img;
-	private int Id=-1;
-	String text="";
+    private int x,y,width,height;
+    private BufferedImage img;
+    private int Id=-1;
+    private float alpha=1f;
+    String text="";
     Font font=new Font("Consolas",Font.PLAIN,20);
     Color textColor=Color.WHITE;
     Thread thr;
-    public BufferedImg(String imgSrc,int locX,int locY,int sizeWidth,int sizeHeight,String content,Font font,Color textColor) {
+    public float getAlpha() {
+        return alpha;
+    }
+    public void setAlpha(float alpha) {
+        if(alpha>1) {
+            alpha=1f;
+        }else if(alpha<0) {
+            alpha=0f;
+        }
+        this.alpha = alpha;
+    }
+    public BufferedImg(String imgSrc,int locX,int locY,int sizeWidth,int sizeHeight,String content,Font font,Color textColor) throws IOException {
         setImg(imgSrc);
         setX(locX);
         setY(locY);
@@ -136,14 +150,14 @@ class BufferedImg implements Runnable{
         setFont(font);
         setTextColor(textColor);
     }
-    public BufferedImg(String imgSrc,int locX,int locY,int sizeWidth,int sizeHeight) {
+    public BufferedImg(String imgSrc,int locX,int locY,int sizeWidth,int sizeHeight) throws IOException {
         setImg(imgSrc);
         setX(locX);
         setY(locY);
         setWidth(sizeWidth);
         setHeight(sizeHeight);
     }
-    public BufferedImg(String imgSrc,int locX,int locY) {
+    public BufferedImg(String imgSrc,int locX,int locY) throws IOException {
         setImg(imgSrc);
         setX(locX);
         setY(locY);
@@ -169,62 +183,62 @@ class BufferedImg implements Runnable{
     public void setTextColor(Color textColor) {
         this.textColor = textColor;
     }
-	public int getId() {
-		return Id;
-	}
-	public void setId(int id) {
-		Id=id;
-	}
-	public int getX() {
-		return x;
-	}
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id=id;
+    }
+    public int getX() {
+        return x;
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public int getY() {
-		return y;
-	}
+    public int getY() {
+        return y;
+    }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public int getWidth() {
-		return width;
-	}
+    public int getWidth() {
+        return width;
+    }
 
-	public void setWidth(int width) {
-		this.width = width;
-	}
+    public void setWidth(int width) {
+        this.width = width;
+    }
 
-	public int getHeight() {
-		return height;
-	}
+    public int getHeight() {
+        return height;
+    }
 
-	public void setHeight(int height) {
-		this.height = height;
-	}
+    public void setHeight(int height) {
+        this.height = height;
+    }
 
-	public Image getImg() {
-		return img;
-	}
+    public BufferedImage getImg() {
+        return img;
+    }
 
-	public void setImg(String imgSrc) {
-		this.img=new ImageIcon(imgSrc).getImage();
-	}
+    public void setImg(String imgSrc) throws IOException {
+        this.img=ImageIO.read(new File(imgSrc));
+    }
 
 
-	
-	public void action() {
+
+    public void action() {
         thr=new Thread(this);
         thr.start();
     }
     @Override
-    
+
     public void run() {
-        
+
     }
 }
 
--- src/screen/TESTUI.java
+++ src/screen/TESTUI.java
@@ -3,23 +3,19 @@ package screen;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Point;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.util.Random;
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class TESTUI {
-	public static void main(String args[]) {//TODO main method
+	public static void main(String args[]) throws IOException {//TODO main method
 		GraphicViewer gV1=new GraphicViewer(100,500,500);
 		gV1.setLocation(new Point(0,0));
-		GraphicViewer gV2=new GraphicViewer(100,500,500);
-		gV2.setLocation(new Point(100,100));
 		ExampleJFrame frame=new ExampleJFrame();
-		BufferedImg danos=new BufferedImg("danos.jpg",0, 0, 1000, 1000);
-		ExampleButton garageDoor=new ExampleButton("inside.png", 84, 352, 840, 458,"Yess!",new Font("Consolas",Font.BOLD,100),Color.red);
-		BufferedImg garageBack=new BufferedImg("outside.png", 0, 0, 1000, 1000);
+		BufferedImg danos=new BufferedImg("danos.jpg",0, 0, 500, 500);
+		ExampleButton garageDoor=new ExampleButton("inside.png", 42, 178, 420, 225,"Yess!",new Font("Consolas",Font.BOLD,100),Color.red);
+		BufferedImg garageBack=new BufferedImg("outside.png", 0, 0, 500, 500);
 		frame.add(gV1);
 		gV1.addButton(danos);
 		gV1.addButton(garageBack);
@@ -29,7 +25,7 @@ public class TESTUI {
 }
 class ExampleButton extends BufferedImg{
 	public ExampleButton(String imgSrc, int locX, int locY, int sizeWidth, int sizeHeight, String content, Font font,
-            Color textColor) {
+            Color textColor) throws IOException {
         super(imgSrc, locX, locY, sizeWidth, sizeHeight, content, font, textColor);
         // TODO Auto-generated constructor stub
     }
@@ -37,11 +33,13 @@ class ExampleButton extends BufferedImg{
 	boolean isOpened=false;
     @Override
 	public void run() {
-		int k=440;
-		int v=50;
+		float k1=1f;
+		float v1=0.05f;
+		int v2=10;
 		if(!isOpened) {
-			for(;k>0;k-=v) {
-				this.setY(this.getY()-v);
+			for(;k1>0;k1-=v1) {
+				this.setAlpha(this.getAlpha()-v1);
+				this.setY(this.getY()-v2);
 				try {
 					Thread.sleep(40);
 				} catch (InterruptedException e) {
@@ -50,8 +48,9 @@ class ExampleButton extends BufferedImg{
 			}
 			isOpened=true;
 		}else {
-			for(;k>0;k-=v) {
-				this.setY(this.getY()+v);
+			for(;k1>0;k1-=v1) {
+				this.setAlpha(this.getAlpha()+v1);
+				this.setY(this.getY()+v2);
 				try {
 					Thread.sleep(40);
 				} catch (InterruptedException e) {
@@ -66,6 +65,6 @@ class ExampleJFrame extends JFrame{
 	public ExampleJFrame() {
 		this.setTitle("custom canvas test");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setSize(1010,1040);
+		this.setSize(510,540);
 	}
 }
