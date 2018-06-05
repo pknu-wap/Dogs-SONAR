85f9eb4208dbcd0d59e052a48254e46e92941c85
JeongbinPark <qkrwjdqls99@gmail.com>
2018-06-06 00:35:30 +0900

    Add thread for moving many buttons(w/ error)

--- src/practiceForGUI/ButtonMoving.java
+++ src/practiceForGUI/ButtonMoving.java
@@ -1,66 +1,102 @@
 package practiceForGUI;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
+import javax.swing.*;
 
-
-
-class Frame extends JFrame {
-	public Frame(){
-		setSize(500,500);
-		setVisible(true);
-		setTitle("buttons");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+class MyRunnable implements Runnable {
+	int num;
+	JFrame f;
+	public MyRunnable(JFrame jframe) {
+		f = jframe;
+		num = (int) (Math.random() * 5 + 1);
+		System.out.println(num + " 출력, " + num  + "번 버튼 Moving");
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//
+//		}
 	}
-	
-}
 
-public class ButtonMoving {
-
-	
-	public static void main(String[] args) throws InterruptedException {
-		Frame f = new Frame();
-
-
+	public void run() {
 		
+
 		JButton bt1 = new JButton("1");
 		JButton bt2 = new JButton("2");
 		JButton bt3 = new JButton("3");
 		JButton bt4 = new JButton("4");
+		JButton bt5 = new JButton("0");
+
+		int speed = (int) (Math.random() * 10 + 1);
+
+		if (num  == 1) {
+			for (int i = 0; i < 1000; i++) {
+				bt1.setBounds(i, 0, 100, 100);
+				f.add(bt1);
+				try {
+					Thread.sleep(speed);
+				} catch (InterruptedException e) {
+				}
+			}
+		} else if (num  == 2) {
+			for (int i = 0; i < 1000; i++) {
+				bt2.setBounds(i, 100, 100, 100);
+				f.add(bt2);
+				try {
+					Thread.sleep(speed);
+				} catch (InterruptedException e) {
+				}
+			}
+		} else if (num  == 3) {
+			for (int i = 0; i < 1000; i++) {
+				bt3.setBounds(i, 200, 100, 100);
+				f.add(bt3);
+				try {
+					Thread.sleep(speed);
+				} catch (InterruptedException e) {
+				}
+			}
+		} else if (num == 4) {
+			for (int i = 0; i < 1000; i++) {
+				bt4.setBounds(i, 300, 100, 100);
+				f.add(bt4);
+				try {
+					Thread.sleep(speed);
+				} catch (InterruptedException e) {
+				}
+			}
+		} else if (num  == 0) {
+			for (int i = 0; i < 1000; i++) {
+				bt5.setBounds(i, 400, 100, 100);
+				f.add(bt5);
+				try {
+					Thread.sleep(speed);
+				} catch (InterruptedException e) {
+				}
+			}
+		} else {
+		}
+
+	}
+}
+
+public class ButtonMoving {
+
+	public static void main(String[] args) throws InterruptedException {
+		JFrame f = new JFrame();
+		f.setSize(1000, 600);
+		f.setVisible(true);
+		f.setTitle("buttons");
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		while(true)
-		{
-		int num = (int) (Math.random()*10+1);
-		int speed = (int) (Math.random()*10+1);
-			
-		if(num==1)
-		{for(int i=0; i<500; i++)
-		{bt1.setBounds(i, 0, 100,100);
-		f.add(bt1);
-		Thread.sleep(speed);
-		}}
-		else if(num==2)
-		{for(int i=0; i<500; i++)
-		{bt2.setBounds(i, 100, 100,100);
-		f.add(bt2);
-		Thread.sleep(speed);
-		}}
-		else if(num==3)
-		{for(int i=0; i<500; i++)
-		{bt3.setBounds(i, 200, 100,100);
-		f.add(bt3);
-		Thread.sleep(speed);
-		}}
-		else if(num==4)
-		{for(int i=0; i<500; i++)
-		{bt4.setBounds(i, 300, 100,100);
-		f.add(bt4);
-		Thread.sleep(speed);
-		}}}
-		
-		
+		for(int i=0; i<5; i++){
+			new Thread(new MyRunnable(f)).start();
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+
+			}
+		}
+		}
+
 	}
 
 
-}
