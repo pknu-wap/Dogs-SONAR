c5608919c0fb5a6f6a332310d344f3d0039f32c5
Jaeguins <jsu4838@naver.com>
2018-06-05 13:26:45 +0900

    sound using example modified

--- src/Exgame/ExGame.java
+++ src/Exgame/ExGame.java
@@ -8,36 +8,50 @@ import soundCapture.*;
 public class ExGame extends JFrame{
 	int random = (int)(Math.random()*10);
 	ImageIcon iicon[];
-	JButton btn[];
+    JButton btn[];
 	BorderLayout layout = new BorderLayout();
-	
+	LevelMeter capture;
+	Reactor actor;
 	ExGame(){
 		super("Exgame");
+		btn = new JButton[2];
 		setLayout(layout);
 		iicon = new ImageIcon[2];
-		iicon[0]=new ImageIcon(new ImageIcon("악당.jpg").getImage().getScaledInstance(150, 200, Image.SCALE_FAST));
-		iicon[1]=new ImageIcon(new ImageIcon("주인공.png").getImage().getScaledInstance(150, 200, Image.SCALE_FAST));
-		btn = new JButton[2];
-		btn[0]=new JButton(iicon[0]);
-		btn[1]=new JButton(iicon[1]);
-		btn[0].setSize(150,200);
-		btn[1].setSize(150,200);
-		if(random>5) {
-			add(btn[0],BorderLayout.WEST);}
-		else
-			add(btn[1],BorderLayout.EAST);
+        iicon[0]=new ImageIcon(new ImageIcon("악당.jpg").getImage().getScaledInstance(150, 200, Image.SCALE_FAST));
+        iicon[1]=new ImageIcon(new ImageIcon("주인공.png").getImage().getScaledInstance(150, 200, Image.SCALE_FAST));
+        btn[0]=new JButton(iicon[0]);
+        btn[1]=new JButton(iicon[1]);
+        btn[0].setSize(150,200);
+        btn[1].setSize(150,200);
+        add(btn[0],BorderLayout.WEST);
+        add(btn[1],BorderLayout.EAST);
+        actor=new Reactor(btn[0],btn[1]);
+        capture=new LevelMeter(actor,0.08,3);
 		setSize(300,200);
 		setVisible(true);
 		System.out.println(random);
 	}
 	public static void main(String args[]) {
 		new ExGame();
+		
 		}
-	class ExampleSoundHandler implements soundHandler{
-
+	class Reactor implements SoundHandler{
+	    JButton me,enemy;
+	    public Reactor(JButton me,JButton enemy) {
+	        this.me=me;
+	        this.enemy=enemy;
+	        
+	    }
 	    @Override
 	    public void action(double now,double peak) {
-	        System.out.println("now : "+(int)now+" peak : "+(int)peak);
+	        if(now>150) {
+	            me.setVisible(false);
+	            enemy.setVisible(true);
+	        }else {
+	            me.setVisible(true);
+	            enemy.setVisible(false);
+	        }
+	            
 	    }
 	    
 	}
