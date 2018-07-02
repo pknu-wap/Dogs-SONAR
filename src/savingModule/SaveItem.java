10ecb4fc92bb453bd36400d3515928bee141d092
Jaeguins <jsu4838@naver.com>
2018-07-02 22:34:42 +0900

    add saveModule

---
+++ src/savingModule/SaveItem.java
@@ -0,0 +1,56 @@
+package savingModule;
+public class SaveItem {
+	enum Type{
+		INT,DOUBLE,STRING;
+	}
+	public String key;
+	Type type;
+	String valueString;
+	int valueInt;
+	double valueDouble;
+	public SaveItem(String key,String value) {
+		this.key=key;
+		this.valueString=value;
+		type=Type.STRING;
+	}
+	public SaveItem(String key,double value) {
+		this.key=key;
+		this.valueDouble=value;
+		type=Type.DOUBLE;
+	}
+	public SaveItem(String key,int value) {
+		this.key=key;
+		this.valueInt=value;
+		type=Type.INT;
+	}
+	public String getKey() {
+		return key;
+	}
+	public String getValueString() {
+		return valueString;
+	}
+	public void setValueString(String valueString) {
+		this.valueString = valueString;
+	}
+	public int getValueInt() {
+		return valueInt;
+	}
+	public void setValueInt(int valueInt) {
+		this.valueInt = valueInt;
+	}
+	public double getValueDouble() {
+		return valueDouble;
+	}
+	public void setValueDouble(double valueDouble) {
+		this.valueDouble = valueDouble;
+	}
+	public void setKey(String key) {
+		this.key = key;
+	}
+	public void setType(Type type) {
+		this.type = type;
+	}
+	public Type getType() {
+		return type;
+	}
+}
---
+++ src/savingModule/SaveManager.java
@@ -0,0 +1,92 @@
+package savingModule;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SaveManager {
+	int count=0;
+	ArrayList<SaveItem> arr;
+	public SaveManager() {
+		arr=new ArrayList<SaveItem>();
+	}
+	public void load(String src) throws IOException {
+		try {
+			BufferedReader r=new BufferedReader(new FileReader(src));
+			count=Integer.parseInt(r.readLine());
+			for(int i=0;i<count;i++) {
+				SaveItem t;
+				String key=r.readLine();
+				switch(Integer.parseInt(r.readLine())) {
+				case 0://string
+					t=new SaveItem(key,r.readLine());
+					break;
+				case 1://integer
+					t=new SaveItem(key,Integer.parseInt(r.readLine()));
+					break;
+				case 2://double
+					t=new SaveItem(key,Double.parseDouble(r.readLine()));
+					break;
+				default:
+					t=new SaveItem(key,null);
+					break;
+				}
+				arr.add(t);
+			}
+			r.close();
+		}catch(FileNotFoundException e) {
+			PrintWriter pw = new PrintWriter(src);
+			pw.println(0);
+			pw.close();
+			this.load(src);
+		}
+	}
+	public void add(SaveItem sv) {
+		arr.add(sv);
+	}
+	public void delete(String key) {
+		for(int i=0;i<count;i++) {
+			SaveItem t=arr.get(i);
+			if(t.key.equals(key)) {
+				arr.remove(i);
+			}
+		}
+	}
+	public void save(String src) {
+		try {
+			PrintWriter pw = new PrintWriter(src);
+			pw.println(count);
+			for(int i=0;i<count;i++) {
+				SaveItem t = arr.get(i);
+				pw.println(t.getKey());
+				switch(t.getType()) {
+				case INT:
+					pw.println(1);
+					pw.println(t.getValueInt());
+					break;
+				case DOUBLE:
+					pw.println(2);
+					pw.println(t.getValueDouble());
+					break;
+				case STRING:
+					pw.println(0);
+					pw.println(t.getValueString());
+					break;
+				default:
+					break;
+				}
+			}
+			pw.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void main(String args[]) {
+		Scanner scan=new Scanner(System.in);
+		SaveManager sv=new SaveManager();
+		
+		scan.nextLine();
+		
+	}
+}
